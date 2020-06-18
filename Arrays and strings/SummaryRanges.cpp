20ff45231e101704fd7a9fff519847dc8bb0c8a6
Jaiskid <neeraj001jaiswal@gmail.com>
2020-06-18 17:03:15 +0530

    codeupdate

---
+++ Arrays and strings/SummaryRanges.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define si second
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define AS 200001
+#define mod 1000000007
+vector<string> SummaryRanges(vector<int>&nums) {
+	int n = nums.size();
+	vector<string>ans;
+	int i = 0;
+	while (i < n) {
+		string temp = "";
+		temp += to_string(nums[i]);
+		int num = nums[i];
+		while (i + 1 < n and nums[i + 1] == nums[i] + 1) {
+			i++;
+		}
+		if (nums[i] != num) {
+			temp += "->";
+			temp += to_string(nums[i]);
+		}
+		ans.push_back(temp);
+		i++;
+	}
+	return ans;
+}
+int main() {
+	fastIO
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int n;
+	cin >> n;
+	vector<int>nums;
+	nums.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> nums[i];
+	}
+	vector<string>ans = SummaryRanges(nums);
+}
